60fb2080bd80c11214d80066af918b6d71ee9e85
Ethan <epestellmoyer@gmail.com>
2017-12-12 16:37:48 -0500

    Added scipy test branch

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import xlrd
-book = xlrd.open_workbook('R8_2018_HS_Coding.xls')
-values = book.sheet_by_index(0)
-velocity = []
-#For loop is to calculate velocity. Prints every given velocity.
-for i in range(1, values.nrows):
-    velocity.append((values.cell(i,1).value)/(values.cell(i,0).value))
-    print(velocity[i-1])
-print(values.nrows)
+import os
+import scipy
+print("scipy version: "+scipy.__version__)
+
+
+os.system("pause")
